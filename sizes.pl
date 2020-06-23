ecdc3321d6c49cbf3ca7fe2ef33bd9cb54906811
akinbowale <akinbowale.akt@gmail.com>
2020-06-23 23:30:54 +0100

    chore: comments

--- sizes.pl
+++ sizes.pl
@@ -5,5 +5,8 @@ bigger(cat, mouse).
 bigger(horse, cat).
 bigger(whale, horse).
 
+% this rule states, variable A is bigger than variable B if there is a fact - bigger(A, B)
 bigger_than(A, B) :- bigger(A, B).
+
+% this rule states, variable A is bigger than variable B if there is a fact - bigger(A, X) and a rule bigger_than(X, B)
 bigger_than(A, B) :- bigger(A, X), bigger_than(X, B).
